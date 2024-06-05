e0b7132c57371f677932325d776fb9ec9c890b3e
acer <acer@DESKTOP-737UGPB>
2024-06-05 16:32:00 +0630

    Add project files.

---
+++ AceInternBatch.RestApi/Controllers/BlogController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AceInternBatch.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            return Ok("GetBlog");
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlogs()
+        {
+            return Ok("CreateBlog");
+        }
+        [HttpPut]
+        public IActionResult UpdateBlogs()
+        {
+            return Ok("UpdateBlog");
+        }
+        [HttpPatch]
+        public IActionResult PatchBlogs()
+        {
+            return Ok("PatchBlog");
+        }
+        [HttpDelete]
+        public IActionResult DeleteBlogs()
+        {
+            return Ok("DeleteBlogs");
+        }
+
+    }
+}
+
+    
